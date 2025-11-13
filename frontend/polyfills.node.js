00d2f300b260585bd3b129e46e4914ec4fbd6795
Avinash Bathula <bathulaavi@gmail.com>
2025-11-13 07:40:17 +0530

    fix: Expo publish polyfill fix

---
+++ frontend/polyfills.node.js
@@ -0,0 +1,5 @@
+// polyfills.node.js
+if (typeof globalThis.requestAnimationFrame === 'undefined') {
+  globalThis.requestAnimationFrame = (cb) => setTimeout(cb, 0);
+  globalThis.cancelAnimationFrame = (id) => clearTimeout(id);
+}
